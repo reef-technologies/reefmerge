e9aabc61bb60f7ab782b916c92684f2d213bf36e
Grzegorz Konefał <grzegorz.konefal@reef.pl>
2017-09-30 23:20:38 +0200

    UNDONE fixing counting conflicts with more than 2 parents

--- countConflicts.sh
+++ countConflicts.sh
@@ -14,17 +14,21 @@ do
 		echo "    $hash"
 	done
 
-	ancestor=$(git merge-base ${A[0]} ${A[1]})
-	git merge-tree $ancestor ${A[0]} ${A[1]} | grep ">>>>>>>" # FIXME works only for 2 parents of commit
-	RES=$(echo $?)
-	let ALL+=1
-	if (( $RES == 0 ))
-	then
-		let COUNT+=1
-		echo "!!!!!!!!! Conflict detected, conflicts = $COUNT, all = $ALL !!!!!!!!"
-	fi
+    if (( ${#A[@]} > 2 ))
+    then
+        echo "Harder case, not implemented yet"
+    else
+        ancestor=$(git merge-base ${A[0]} ${A[1]}) # works only for 2 parents of commit
+        git merge-tree $ancestor ${A[0]} ${A[1]} | grep ">>>>>>>" # FIXME this grepping may not be sufficient
+        RES=$(echo $?)
+        let ALL+=1
+        if (( $RES == 0 ))
+        then
+            let COUNT+=1
+            echo "!!!!!!!!! Conflict detected, conflicts = $COUNT, all = $ALL !!!!!!!!"
+        fi
+    fi
 done
 
 echo "all merges: $ALL"
 echo "conflicts: $COUNT"
-
